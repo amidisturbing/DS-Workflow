57c24bf65d4e2833ef0b9c27f2d83699929d38b8
Rafa <rafaelaneff@posteo.de>
2018-05-27 21:52:34 +0200

    cleaning script for input.csv done

--- src/cleaner.py
+++ src/cleaner.py
@@ -7,22 +7,16 @@ def check_input(input):
 		return True
 
 def clean_age(age):
-	data = age
 	try:
-		if 0<=data<=120:
-			print("WTF?")
-			if int(data)<=-1:
-				data = abs(int(row['age']))
+		#if the age is negative, assume the absolute value is the correct one
+		if int(age)<=(-1):
+			age = abs(int(age))
 	except:
-		data =='NAN'
+		age =='NAN'
+		#if there is no age set, use string for field and instruct customer service to call client for age information
 		return 'NO_AGE'
-	return data
+	return age
 
-def dict_filter(it, *keys):
-	for d in it:
-		yield dict((k, d[k]) for k in keys)
-
-#def check_duplicates(rows_without_id):
 
 
 input_data = 'input.csv'
@@ -30,31 +24,37 @@ input_data = 'input.csv'
 def clean_data(data):
 	csv_file = data
 	f = open(csv_file)
-	out = open('first_edit.csv', 'w')
+	out = open('output.csv', 'w')
 	fieldnames = ['id','full_name','first_name','last_name','email','gender','age']
 	writer = csv.DictWriter(out, fieldnames=fieldnames)
 	writer.writeheader()
 	CSV = csv.DictReader(f)
-	seen_ids = {}
-	i = 0
+	entries = set()
+	i = 1
 	for row in CSV:
 		print(row)
 		email = row['email']
 		gender = row['gender']
-		if check_input(row['full_name']):
-			full_name=row['full_name']
-			first_name = row['first_name']
-			last_name = row['last_name']
-		if not check_input(row['full_name']):
-			#row['first_name']='NO_NAME'
-			print ("No Name -> DELETE!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		full_name=row['full_name']
+		first_name = row['first_name']
+		last_name = row['last_name']
+		#if there is no name set for the user the information in the row is useless  => Delete.
+		if not check_input(full_name or first_name or last_name ):
 			continue
 		if not check_input(email):
-			email = 'NO_EMAIL'
+			#if there is no email set, assume client has a defaut email on our domain
+			email = first_name + '.' + last_name + '@defaultemail.com'
 		if not check_input(gender):
+			#if there is no gender set, use string for field and instruct customer service to call client for gender information
 			gender = 'NO_GENDER'
 		age=clean_age(row['age'])
-		writer.writerow({'id':row['id'],'full_name':full_name,'first_name':first_name,'last_name':last_name,'email':email,'gender':gender,'age':age})
+
+		entry = '%s,%s,%s,%s,%s,%s' % (full_name,first_name,last_name,email,gender,age)
+		if entry not in entries:
+			#write entry with new id fitting the iteration
+			writer.writerow({'id':i,'full_name':full_name,'first_name':first_name,'last_name':last_name,'email':email,'gender':gender,'age':age})
+			entries.add(entry)
+		#iter count used as id
 		i = i+1
 
 	f.close()
